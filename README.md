d284c53d224c8b8bb3471a6e602e7e14323b2e2d
hyungjin kim <hjux07@gmail.com>
2024-06-21 15:55:23 +0900

    part 1: Vue 이벤트 핸들러로 click 감지하기 - ` @이벤트명=""` 이런식으로 이벤트 바인딩    ex) @click="", v-on:click="", .. - vue에서 사용할 데이터들은 `data()` 메서드의 반환하는 객체에 세팅 - vue에서 함수를 선언하려면 `methods` 속성에 함수를 선언 - 함수 안에서 data 사용할 때 this를 사용해 접근 - vue는 데이터가 변하면 관련된 HTML은 바로 반영되어 자동으로 바뀜

--- README.md
+++ README.md
@@ -5,3 +5,38 @@
 - HTML 속성에 데이터 바인딩 할 때 속성 왼쪽에 `:` 삽입
 
 - vue의 HTML 반복문 : `<태그 v-for="(순회 요소, index) in 반복 횟수 || 순회할 자료형 데이터" :key="key값">`
+
+- vue에서 이벤트 바인딩할 때 `@이벤트명=""` 방식으로 사용
+
+  ```javascript
+  ex) @click="", v-on:click="" // 둘다 똑같다 @는 v-on의 축약형
+  ```
+
+- vue는 데이터가 변하면 관련된 HTML은 바로 반영되어 자동으로 바뀜
+
+- vue에서 사용할 데이터들은 `data()` 메서드의 반환하는 객체에 세팅
+
+- vue에서 함수를 선언하려면 `methods` 속성에 함수를 선언
+
+```javascript
+  // 데이터 보관함
+  data() {
+    return {      
+      products: [
+        { room: '역삼동 원룸', price: 50, declaration: 0 },
+        ...
+      ],
+    };
+  },
+  // 함수 선언하는 공간
+  methods: {
+    increase(i) {
+      /**
+       * vue에서 함수 선언할 때 주의사항
+       * - 함수 안에서 data 사용할 때 this를 사용해 접근
+       */
+      this.products[i].declaration += 1;
+    },
+  },
+
+```
--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="container">
-    <div class="menu">
-      <!-- vue의 HTML 반복문 : <태그 v-for="(순회 요소, index) in 반복 횟수 || 순회할 자료형 데이터" :key="key값"> -->
-      <a v-for="(item, index) in menuList" :key="index" href="#">{{ item }}, {{ index }}</a>
+    <div class="container">
+        <div class="menu">
+            <!-- vue의 HTML 반복문 : <태그 v-for="(순회 요소, index) in 반복 횟수 || 순회할 자료형 데이터" :key="key값"> -->
+            <a v-for="(item, index) in menuList" :key="index" href="#">{{ item }}, {{ index }}</a>
+        </div>
+    
+        <img src="@assets/vue.svg" alt="vue logo" />
+    
+        <div class="wrap">
+            <h2>원룸샵</h2>
+    
+            <div v-for="({ room, price }, i) in products" :key="i">
+                <h4 :style="fontColor">{{ products[i].room }}</h4>
+                <p>{{ products[i].price }}</p>
+                <!--                   
+                        @이벤트명="" 이런식으로 이벤트 바인딩
+                        ex) @click="", v-on:click="", @mouseover="" ..                  
+                    -->
+                <button type="button" @click="increase(i)">허위매물 신고</button>
+                <span>신고 수: {{ products[i].declaration }}</span>
+            </div>
+        </div>
     </div>
-
-    <img src="@assets/vue.svg" alt="vue logo" />
-
-    <div class="wrap">
-      <h2>원룸샵</h2>
-      <div v-for="({ room, price }, i) in products" :key="i">
-        <h4 :style="fontColor">{{ room }}</h4>
-        <p>{{ price }}</p>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -23,51 +30,57 @@
  * 2. {{데이터 바인딩}}
  */
 export default {
-  name: 'App',
-  // 데이터 보관함
-  data() {
-    return {
-      menuList: ['Home', 'shop', 'About'],
-      fontColor: 'color: blue',
-      products: [
-        { room: '역삼동 원룸', price: 50 },
-        { room: '천호동 원룸', price: 70 },
-        { room: '마포구 원룸', price: 30 },
-      ],
-    };
-  },
+    name: 'App',
+    // 데이터 보관함
+    data() {
+        return {
+            menuList: ['Home', 'shop', 'About'],
+            fontColor: 'color: blue',
+            products: [
+                { room: '역삼동 원룸', price: 50, declaration: 0 },
+                { room: '천호동 원룸', price: 70, declaration: 0 },
+                { room: '마포구 원룸', price: 30, declaration: 0 },
+            ],
+        };
+    },
+    // 함수 선언하는 공간
+    methods: {
+        increase(i) {
+            /**
+             * vue에서 함수 선언할 때 주의사항
+             * - 함수 안에서 data 사용할 때 this를 사용해 접근
+             */
+            // console.log(this);
+            this.products[i].declaration += 1;
+        },
+    },
 };
 </script>
 
 <style>
 .container {
-  max-width: fit-content;
-  margin: 0 auto;
-  text-align: center;
-
-  .menu {
-    background-color: darkslateblue;
-    padding: 15px;
-    border-radius: 5px;
-
-    & > a {
-      color: #fff;
-      padding: 10px;
+    max-width: fit-content;
+    margin: 0 auto;
+    text-align: center;
+    .menu {
+        background-color: darkslateblue;
+        padding: 15px;
+        border-radius: 5px;
+        &>a {
+            color: #fff;
+            padding: 10px;
+        }
     }
-  }
-
-  .wrap {
-    margin-top: 10px;
-
-    & > h2 {
-      margin-bottom: 10px;
-      font-size: 1.4rem;
-      font-weight: bold;
+    .wrap {
+        margin-top: 10px;
+        &>h2 {
+            margin-bottom: 10px;
+            font-size: 1.4rem;
+            font-weight: bold;
+        }
+        &>p {
+            margin-bottom: 14px;
+        }
     }
-
-    & > p {
-      margin-bottom: 14px;
-    }
-  }
 }
 </style>
